c40d2f7f759c59b88d9532a80b095e0efb5792c7
sophy <2njoybliss@gmail.com>
2021-06-28 00:42:39 +0900

    함수 기본 매개변수 처리하기

--- b37.js
+++ b37.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function drawChart(width = 200, height = 400) {
+  console.log(`${width} X ${height} 차트를 그립니다`);
+}
+drawChart(100);
+drawChart();
+
+function drawCahrt2(width = 200, height = width / 2) {
+  console.log(`${width} X ${height} 차트를 그립니다`);
+}
+drawCahrt2(300); //width 에만 값이 적용됨 첫번째 인자값만
+drawCahrt2();
